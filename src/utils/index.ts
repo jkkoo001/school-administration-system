3b52ca167210c562e2de9548f2e6b3c1d6cb94cd
Jan Kheng Koo <jankheng.koo@gmail.com>
2025-09-21 23:22:41 +0800

    Update csv parser to remove file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,33 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 import { CsvItem } from 'CsvItem';
+import Logger from '../config/logger';
 
-export const convertCsvToJson = (filePath: string): Promise<CsvItem[]> => {
+const LOG = new Logger(__filename);
+
+const deleteFile = (path: string) => {
+  fs.unlink(path, (err) => {
+    if (err) {
+      LOG.error(`Failed to delete CSV file ${path}: ${err}`);
+    }
+  });
+};
+
+export const convertCsvToJson = async (filePath: string): Promise<CsvItem[]> => {
   const results: CsvItem[] = [];
   const stream = fs.createReadStream(filePath).pipe(csv());
 
   return new Promise((resolve, reject) => {
     stream.on('data', (data: CsvItem) => results.push(data));
-    stream.on('end', () => resolve(results));
-    stream.on('error', (err) => reject(err));
+    stream.on('end', () => {
+      LOG.info(`Successfully parsed CSV file: ${filePath}`);
+      resolve(results);
+      deleteFile(filePath);
+    });
+    stream.on('error', (error) => {
+      LOG.error(`Error parsing CSV file: ${filePath} ${error}`);
+      deleteFile(filePath);
+      reject(error);
+    });
   });
-}
+};
