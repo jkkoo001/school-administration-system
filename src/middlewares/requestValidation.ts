e3379ee683a63fe76b5c447290e206510841fc25
Jan Kheng Koo <jankheng.koo@gmail.com>
2025-09-21 23:23:56 +0800

    Add request validator error handler

---
+++ src/middlewares/requestValidation.ts
@@ -0,0 +1,20 @@
+import { validationResult } from 'express-validator';
+import { NextFunction, Request, Response } from 'express';
+
+export const handleValidationErrors = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({
+      error: 'Validation failed',
+      details: errors.array(),
+    });
+
+    return;
+  }
+
+  next();
+};
